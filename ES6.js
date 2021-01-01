d2bee7e0d876b34967f4cd4272e9cab520ec7ac8
Shivam Kumar <48478625+shivam-kr216@users.noreply.github.com>
2021-01-01 21:30:20 +0530

    Update ES6.js
    Updated

--- ES6.js
+++ ES6.js
@@ -16,7 +16,8 @@ const product = {
     stock: 201,
     rating: 5
 }
-const {label: productLabel , price, stock, rating=5} = product;
+const {label: productLabel , price, stock, rating=4.2} = product;
+//HERE RATING WILL TAKE VALUE AS 5 NOT THE DEFAULT ONE I.E., 4.2 BECAUSE IN THIS CASE WE ARE PROVIDING THE VALUE 
 console.log(productLabel + ' ' + price + ' ' + stock + ' ' + rating)
 
 const transaction = (type, { label, stock }) => {
