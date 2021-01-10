38782c324ccf945f3bda70d09c22be903271b1fe
Shivam Kumar <48478625+shivam-kr216@users.noreply.github.com>
2021-01-10 09:45:50 +0530

    Added code

--- promise.js
+++ promise.js
@@ -19,3 +19,31 @@ doWorkPromise.then((result)=> {
 //                                           \
 //                                            \
 //                                           rejected
+
+const add = (a,b) => {
+    return new Promise( (resolve, reject) => {
+        setTimeout(()=>{
+            resolve(a+b)
+        },2000)
+    })
+}
+
+//add(1, 2).then((result) => {
+//    console.log(result);
+//    add(result,5).then((sum) => {
+//        console.log(sum);
+//    }).catch((e) => {
+//        console.log(e);
+//    })
+//}).catch((e)=>{
+//    console.log(e);
+//})
+
+add(1,1).then((sum) => {
+    console.log(sum);
+    return add(sum, 6)
+}).then((sum) => {
+    console.log(sum)
+}).catch((e) => {
+    console.log(e);
+})
